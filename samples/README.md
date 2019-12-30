c35e14f305bcae920b0c0a7ed60db1ab9e32a40f
SecAuth <59008726+secauth@users.noreply.github.com>
2019-12-30 10:09:34 +0800

    Add files via upload

---
+++ samples/README.md
@@ -0,0 +1,5 @@
+﻿[![微信小程序「安全登录」logo](https://cdn.jsdelivr.net/gh/secauth/docs@master/assets/secauth-logo.png)](https://github.com/secauth/docs)
+
+# 安全登录样例
+
+希望能帮助你快速理解「安全登录」的运行机制。
---
+++ samples/nodejs/index.js
@@ -0,0 +1,34 @@
+﻿const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
+
+// 令牌密钥
+const secret = '';
+// 访问令牌
+const accessToken = '';
+
+// 获取小程序码
+const getWxaCode = async (id) => {
+  const res = await fetch(`https://service-ggnj6gz0-1256804704.ap-hongkong.apigateway.myqcloud.com/release/wxacode?id=${id}`, {
+    headers: { 'Authorization': `Bearer ${accessToken}` },
+  });
+  const body = await res.json();
+  return body.data;
+};
+
+getWxaCode('abcd1234'); // 字符串 1-24 位
+
+// 登录回调
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  const { token } = req.body;
+  const { id, username, password } = jwt.verify(token, secret);
+  res.json({
+    status: 'success' // 登录成功
+  });
+});
+
+app.listen(3000);
